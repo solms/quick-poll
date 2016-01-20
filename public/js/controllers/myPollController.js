b190a2c1c47f11e752a2f378b8f064f409cc6865
Solms Dykman <solmsd@gmail.com>
2016-01-20 23:47:15 +0900

    My Poll draws a simple, working bar chart

--- public/js/controllers/myPollController.js
+++ public/js/controllers/myPollController.js
@@ -9,7 +9,23 @@ angular.module('myPollController', [])
 				for(var i=0; i<$scope.poll.votes.length; i++) {
 					$scope.poll.total_votes += $scope.poll.votes[i];
 				}
+				drawGraph();
 			}, function(response) {		// Failure
 				$scope.problem = 'Could not load the poll data from the server :(';
 			})
+
+		// Visualize the voting data using D3.js
+		var drawGraph = function() {
+			// Scale the x-axis of the graph
+			var x = d3.scale.linear()
+		    .domain([0, d3.max($scope.poll.votes)])
+		    .range([0, 500]);
+
+			d3.select('#graph')
+				.selectAll('div')
+					.data($scope.poll.votes)
+				.enter().append('div')
+					.style('width', function(d) { return x(d)+'px'; })
+					.text(function(d) { return d; });
+		}
 	}]);
