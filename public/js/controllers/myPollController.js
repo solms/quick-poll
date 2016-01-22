716d94a85b7bb33995154e1a09b5d247dbb4828d
Solms Dykman <solmsd@gmail.com>
2016-01-22 13:13:46 +0900

    Vertical graph works; No proper lables

--- public/js/controllers/myPollController.js
+++ public/js/controllers/myPollController.js
@@ -16,30 +16,33 @@ angular.module('myPollController', [])
 
 		// Visualize the voting data using D3.js
 		var drawGraph = function() {
-			var width 		= 500,
-				barHeight 	= 20;
+			var width 	= 750,
+				height 	= 350;
 
-			var x = d3.scale.linear()
+			var y = d3.scale.linear()
 				.domain([0, d3.max($scope.poll.votes)])
-				.range([0, width]);
+				.range ([height, 0]);
 
 			var chart = d3.select('.chart')
-				.attr('width' , width)
-				.attr('height', barHeight * $scope.poll.votes.length);
+				.attr('width',  width)
+				.attr('height', height);
+
+			var barWidth = width / $scope.poll.votes.length;
 
 			var bar = chart.selectAll('g')
-				.data($scope.poll.votes)
-				.enter().append('g')
-				.attr("transform", function(d, i) { return "translate(0," + i * barHeight + ")"; });
+			  .data($scope.poll.votes)
+			.enter().append('g')
+			  .attr('transform', function(d, i) { return 'translate(' + i * barWidth + ',0)'; });
 
 			bar.append('rect')
-				.attr('width', x)
-				.attr('height', barHeight - 1);
+			  .attr('y', function(d) { return y(d); })
+			  .attr('height', function(d) { return height - y(d); })
+			  .attr('width', barWidth - 1);
 
-			bar.append("text")
-			    .attr("x", function(d) { return x(d) - 3; })
-			    .attr("y", barHeight / 2)
-			    .attr("dy", ".35em")
-			    .text(function(d) { return d; });
+			bar.append('text')
+			  .attr('x', barWidth / 2)
+			  .attr('y', function(d) { return y(d) + 3; })
+			  .attr('dy', ".75em")
+			  .text(function(d) { return d; });
 		}
 	}]);
