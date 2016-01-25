13fdf2cb44698921e8a1ec816384de7e6cbf0374
Solms Dykman <solmsd@gmail.com>
2016-01-25 14:15:06 +0900

    Updated to use cloud-based database

--- app/routes.js
+++ app/routes.js
@@ -153,7 +153,6 @@ module.exports = function(app, passport) {
 					}
 				});
 				doc.markModified('options');
-				doc.markModified('voted');
 				doc.save();
 				res.status(200);			
 			} else {
--- config/db.js
+++ config/db.js
@@ -1,3 +1,4 @@
 module.exports = {
-	url: 'mongodb://localhost:27017/quick-poll'
+	url: 'mongodb://solms:quick-poll@ds031893.mongolab.com:31893/quick-poll'
+	//url: 'mongodb://localhost:27017/quick-poll'
 };
